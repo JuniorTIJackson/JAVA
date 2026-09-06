a074b92e7e5c9986d498f6211bae2cc81332cfd3
JuniorTIJackson <joao.j.junior@ba.estudante.senai.br>
2026-09-05 22:29:59 -0300

    Add files via upload

---
+++ teste_1.java
@@ -0,0 +1,30 @@
+
+public class teste_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Tabela ");
+		
+		int soma_1=10;
+		
+		double soma_2=3.0;
+		
+		char caractere= 'b';
+		
+		boolean verdadeiro= true;
+		
+		boolean falso= false;
+		
+		float salario= 5000;
+		
+		System.out.println("sua idade é de: " +soma_1+ " Parabéns");
+		System.out.println(soma_2);
+		System.out.println(caractere);
+		System.out.println(verdadeiro);
+		System.out.println(falso);
+		System.out.println(salario);
+		
+
+	}
+
+}
